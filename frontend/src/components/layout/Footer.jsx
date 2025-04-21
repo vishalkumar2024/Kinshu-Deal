8ad120bb1260869d016e3bcaf1dffca9078f9c8f
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-04-21 18:13:05 +0530

    did some modifications

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <>
       <footer className="bg-gray-800 text-white py-4 mt-auto">
         <div className="container mx-auto text-center">
-          <p className="text-sm">&copy; 2023 XYZ Medical Cards. All rights reserved.</p>
-          <p className="text-sm">Developed by Your Name</p>
+          <p className="text-sm">&copy; 2023 MediTransact Medical Cards. All rights reserved.</p>
+          <p className="text-sm">Developed by 404 Found</p>
         </div>
       </footer>
     </>
